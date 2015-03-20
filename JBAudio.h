6853f28e3e49cad28b0a81213e0c5328a1510d3e
beastgrim <beastgrim@gmail.com>
2015-03-20 10:35:53 +0300

    addH

---
+++ JBAudio.h
@@ -0,0 +1,17 @@
+//
+//  JBAudio.h
+//  CRM
+//
+//  Created by Богомолов Евгений on 20.03.15.
+//  Copyright (c) 2015 FOND MAGUTA. All rights reserved.
+//
+
+#ifndef CRM_JBAudio_h
+#define CRM_JBAudio_h
+
+#import "JBAudioPlayer.h"
+#import "JBAudioRecorder.h"
+#import "JBFlacSupport.h"
+
+
+#endif
